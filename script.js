f080aa9d09efe4b7163786a515f1d547010e30d5
lautipetitfour <81997592+lautipetitfour@users.noreply.github.com>
2024-10-25 19:30:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
